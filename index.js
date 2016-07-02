b94c328e435cad6d6f22c1763e1e3fd4b20f88fb
Philip Hutchins <flipture@gmail.com>
2016-07-01 20:24:44 -0400

    adding changes before more chagnes...

--- index.js
+++ index.js
@@ -11,9 +11,20 @@ inherits(FileStream, Readable);
 FileStream.prototype._read = function (size) {
   var self = this;
 
+  console.log('File-Stream got _read, size: %s this.curIndex: %s  ', size, this.curIndex);
+
+  if (this.counter-- === 0) {
+    console.log('END OF DATA STREAM');
+  }
+
   if (!self.content) {
+    // ******************
+    // The problem is that this is never getting called!
+    // ******************
+    console.log('End of data through file-stream');
     self.push(null);
   } else {
+    console.log('Sending data through file-stream');
     var chunkBlob = self.content.slice(0, size);
 
 		blobToBuffer(chunkBlob, function (err, chunkBuffer) {
